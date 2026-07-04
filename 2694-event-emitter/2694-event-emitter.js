873c303c8161fc047b00d4fac88e0339bf2a091e
saurav pandey <144902103+SauravPandey56@users.noreply.github.com>
2026-07-04 12:50:45 +0530

    Time: 56 ms (7.22%), Space: 54.2 MB (50.55%) - LeetHub

---
+++ 2694-event-emitter/2694-event-emitter.js
@@ -0,0 +1,58 @@
+class EventEmitter {
+    constructor() {
+        this.events = new Map();
+    }
+
+    /**
+     * @param {string} eventName
+     * @param {Function} callback
+     * @return {Object}
+     */
+    subscribe(eventName, callback) {
+
+        if (!this.events.has(eventName)) {
+            this.events.set(eventName, []);
+        }
+
+        const callbacks = this.events.get(eventName);
+        callbacks.push(callback);
+
+        return {
+            unsubscribe: () => {
+                const index = callbacks.indexOf(callback);
+                callbacks.splice(index, 1);
+            }
+        };
+    }
+
+    /**
+     * @param {string} eventName
+     * @param {Array} args
+     * @return {Array}
+     */
+    emit(eventName, args = []) {
+
+        if (!this.events.has(eventName)) {
+            return [];
+        }
+
+        const result = [];
+
+        for (const callback of this.events.get(eventName)) {
+            result.push(callback(...args));
+        }
+
+        return result;
+    }
+}
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
