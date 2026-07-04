c4076d604638c2f52c1b0d04f590e33e2a0518fd
saurav pandey <144902103+SauravPandey56@users.noreply.github.com>
2026-07-04 12:48:37 +0530

    Time: 75 ms (79.58%), Space: 74 MB (81.35%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+    const result = [];
+
+    function dfs(current, depth) {
+        for (let item of current) {
+            if (Array.isArray(item) && depth < n) {
+                dfs(item, depth + 1);
+            } else {
+                result.push(item);
+            }
+        }
+    }
+
+    dfs(arr, 0);
+
+    return result;
+};
