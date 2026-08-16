2d28ae39bd6b157dcde04ea193be27a9b9dd41bc
saurav pandey <144902103+SauravPandey56@users.noreply.github.com>
2026-08-16 08:27:28 +0530

    Time: 4 ms (75.68%), Space: 115.1 MB (16.22%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.java
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -6,22 +6,17 @@ class Solution {
             count[stone % 3]++;
         }
 
-        // If there are no stones with remainder 1 or 2,
-        // Alice must eventually take a 0-mod-3 stone and lose.
+    
         if (count[1] == 0 && count[2] == 0) {
             return false;
         }
 
-        // If the number of 0-mod-3 stones is even,
-        // the game depends on whether both types (1 and 2)
-        // are available.
+       
         if (count[0] % 2 == 0) {
             return count[1] > 0 && count[2] > 0;
         }
 
-        // If count[0] is odd:
-        // Alice wins if the imbalance between count[1] and count[2]
-        // is large enough.
+      
         return Math.abs(count[1] - count[2]) > 2;
     }
 }
