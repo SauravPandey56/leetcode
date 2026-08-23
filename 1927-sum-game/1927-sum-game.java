3fabab23677dad9bab6b456cbc70a118c770c5b3
saurav pandey <144902103+SauravPandey56@users.noreply.github.com>
2026-08-23 18:08:47 +0530

    Time: 6 ms (97.08%), Space: 47.1 MB (41.61%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int half = n / 2;
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < half; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                leftQ++;
+            } else {
+                leftSum += c - '0';
+            }
+        }
+
+        for (int i = half; i < n; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                rightQ++;
+            } else {
+                rightSum += c - '0';
+            }
+        }
+
+        // Odd number of '?' means Alice gets the last move
+        if ((leftQ + rightQ) % 2 == 1) {
+            return true;
+        }
+
+        // Bob wins only when this exact signed balance is possible
+        return leftSum - rightSum != 9 * (rightQ - leftQ) / 2;
+    }
+}
